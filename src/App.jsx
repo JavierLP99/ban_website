4cde04c885f3a86bcefa7c96f7d4eab6642a8370
Javier Lagunes <mjavlagunes@gmail.com>
2024-12-29 18:27:28 -0600

    Modification to initial conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
+import RoutesIndex from './routes';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
