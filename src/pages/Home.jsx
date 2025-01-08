9d92122c3f348308d97e47b2da8dd11b32ed3774
Javier Lagunes <mjavlagunes@gmail.com>
2025-01-08 02:27:57 -0600

    Modified home page, modified banner and added MostSold section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import Banner from '@/components/home/Banner.jsx'
+import MostSold from '../components/home/mostsold'
+
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <MostSold/>
     </div>
   )
 }
