5ac119112cb85be03d3cd935a82ca9d2a1c7ae1c
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2025-01-25 12:09:43 +0400

    Added error boundary

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home, About } from '@/pages'
-import ProductDetail from '../pages/ProductDetail'
-
+import { Routes, Route } from 'react-router-dom';
+import { Home, About } from '@/pages';
+import ProductDetail from '../pages/ProductDetail';
+import ErrorBoundary from './ErrorBoundary';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/acercade' element={<About />} />
-      <Route path='/producto' element={<ProductDetail />} />
+      <Route path="/" element={<Home />} errorElement={<ErrorBoundary><h1>Something went wrong.</h1></ErrorBoundary>} />
+      <Route path="/acercade" element={<About />} errorElement={<ErrorBoundary><h1>Something went wrong.</h1></ErrorBoundary>} />
+      <Route path="/producto" element={<ProductDetail />} errorElement={<ErrorBoundary><h1>Something went wrong.</h1></ErrorBoundary>} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
