e642d5fb3f3edbab1f5255985b59975f9722d3c0
Javier Lagunes <mjavlagunes@gmail.com>
2025-01-29 08:50:37 -0600

    Added contact form and packages for its functionality

---
+++ src/components/home/Contact.jsx
@@ -0,0 +1,169 @@
+import { useRef, useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { Modal, Button } from 'react-bootstrap'
+import emailjs from '@emailjs/browser'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const Contact = () => {
+  const schema = yup
+    .object({
+      firstName: yup.string().required('Es necesario ingresar tu nombre'),
+      lastName: yup.string().required('Es necesario ingresar tu apellido'),
+      organization: yup.string(),
+      phone: yup.string(),
+      email: yup.string().required('Ingesa un email para contactarte'),
+      message: yup.string().required('Escribe tu mensaje')
+    })
+    .required()
+
+  const [showModal, setShowModal] = useState(false)
+  const form = useRef()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({ resolver: yupResolver(schema) })
+
+  const handleCloseModal = () => setShowModal(false)
+
+  const whenSubmit = data => {
+    emailjs
+      .sendForm('contact_service', 'contact_form', form.current, {
+        publicKey: 'ZbCsu0DS45Vozgnve'
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!')
+        },
+        error => {
+          console.log('FAILED...', error.text)
+        }
+      )
+
+    const result = { firstName: '', lastName: '', email: '', message: '' }
+    reset(result)
+    setShowModal(true)
+  }
+
+  const [content, setContent] = useState([])
+
+  useEffect(() => {
+    fetch('/home.json') // Adjust the path as needed
+      .then(response => response.json())
+      .then(data => setContent(data.contact))
+      .catch(error => console.error('Error loading content:', error))
+  }, [])
+
+  const ImportDrivePhoto = (driveUrl, height) => {
+    const defaultUrl =
+      'https://drive.google.com/file/d/1Q7By_xG9r3a8Zr47j6b1HG7yAm91GIHO/view?usp=drive_link'
+
+    const match = driveUrl.match(/\/d\/(.*)\//)
+    const fileId = match ? match[1] : defaultUrl.match(/\/d\/(.*)\//)[1]
+
+    const newUrl = `https://lh3.googleusercontent.com/d/${fileId}=h${height}`
+
+    return newUrl
+  }
+
+  if (!content) return <div>Loading...</div>
+
+  return (
+    <div className='py-5'>
+      <div className='container text-center d-flex flex-column align-items-center'>
+        <h2 className='fw-bold fs-5 mb-3 col-6'>{content.title}</h2>
+        <div className='rainbow-divider mb-3'></div>
+      </div>
+      <div className='row justify-content-center'>
+        <p className='text-justify text-center col-5'>{content.description}</p>
+      </div>
+      <div className='contact-container'>
+
+
+        <div className='row mx-auto col-5'>
+        <form
+          onSubmit={handleSubmit(whenSubmit)}
+          ref={form}
+          className='d-block'
+          name='contact'
+        >
+          <div className='d-flex flex-column col-12'>
+            <label htmlFor='email'>Correo</label>
+            <input
+              type='text'
+              name='email'
+              placeholder='correo@mail.com'
+              id='email'
+              {...register('email')}
+              className='my-2 p-2 border border-2'
+            />
+            <p className='text-warning text-center'>{errors.email?.message}</p>
+          </div>
+
+          <div className='d-flex flex-column col-12'>
+            <label htmlFor='phone'>Teléfono (Opcional)</label>
+            <input
+              type='text'
+              name='phone'
+              placeholder='55 1234 5678'
+              id='phone'
+              {...register('phone')}
+              className='my-2 p-2 border border-2'
+            />
+            <p className='text-warning text-center'>{errors.phone?.message}</p>
+          </div>
+
+          <div className='d-flex flex-column col-12'>
+            <label htmlFor='message'>Mensaje</label>
+            <textarea
+              name='message'
+              placeholder='Escribe tu mensaje'
+              id='message'
+              {...register('message')}
+              className='my-2 p-2 border border-2'
+              rows='3'
+            />
+            <p className='text-warning text-center'>
+              {errors.message?.message}
+            </p>
+          </div>
+
+          <div className='text-center'>
+            <button
+              type='submit'
+              className='btn btn-primary rounded-pill text-light my-3'
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+      </div>
+
+
+
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        className='align-self-center'
+        centered
+      >
+        <Modal.Body className='rounded'>
+          <h2 className='text-center'>Gracias por contactarnos</h2>
+          <p className='text-center'>Nos comunicaremos pronto contigo</p>
+
+          <div className='text-center mt-4'>
+            <Button variant='dark' onClick={handleCloseModal}>
+              Cerrar
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+      <div className='row justify-content-center mx-auto mt-4 col-8'></div>
+    </div>
+  )
+}
+
+export default Contact
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
 import Banner from '@/components/home/Banner.jsx'
 import MostSold from '../components/home/mostsold'
-
+import Contact from '../components/home/Contact'
 import Categories from '../components/home/Categories'
 import NewProducts from '../components/home/NewProducts'
 const Home = () => {
-  
-
   return (
     <div>
       <Banner />
       <Categories />
       <NewProducts />
       <MostSold />
-      </div>
+      <Contact />
+    </div>
   )
 }
 
